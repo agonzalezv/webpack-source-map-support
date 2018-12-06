78537039783b6e0672bfbc03df9ab6e711c6ad0f
Andres Gonzalez <>
2018-12-06 16:17:58 +1300

    feat: es6 import

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
 
-const plugin = function() {
+const plugin = function () {
   return new webpack.BannerPlugin({
     banner: 'require("source-map-support").install();',
     raw: true,
     entryOnly: false
-  });
-};
+  })
+}
 
-module.exports = plugin;
+module.exports = plugin
